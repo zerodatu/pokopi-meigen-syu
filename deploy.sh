35706d22ef74ca9248767ae8101062fd45e5b0e0
ziropu <datutarousumile@gmail.com>
2025-11-22 20:00:59 +0900

    deploy.shで自動的にブラウザが出るように修正

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-
-uvicorn index:app --reload --port 8000
+    uvicorn index:app --reload --port 8000 &
+    pid=$!
+    sleep 1  # 起動待ち、必要に応じて伸ばす
+    xdg-open http://127.0.0.1:8000
+    wait $pid
